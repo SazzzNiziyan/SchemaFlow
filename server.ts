9c5fd17f4571ef5d690f5a02a623031311645ec0
Sajal Singh <130673366+SazzzNiziyan@users.noreply.github.com>
2026-07-22 23:59:45 +0530

    Remove server start log and listen function
    Removed server listen function and console log.

--- server.ts
+++ server.ts
@@ -11,10 +11,6 @@ async function startServer() {
   const app = express();
  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
   app.use(express.json());
 
   // API Routes
